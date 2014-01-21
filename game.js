299d70a50115ed6fd96c1513cbc1a4f069155b4a
Fernando Sarres <sarresfernando@gmail.com>
2014-01-21 20:01:50 -0200

    added walk function

--- game.js
+++ game.js
@@ -1,12 +1,31 @@
 t1="Bem-vindo à Ilha de Log!\n" +
 	"Esta ilha sofre de escassez de recursos. Falta de comida e água potável têm sido as principais causas de um caos generalizado." +
-	"Casas são saqueadas, idosos e crianças são assassinados e portas e janelas são reforçadas com pedaços de madeiras em nome da segurança." +
+	" Casas são saqueadas, idosos e crianças são assassinados e portas e janelas são reforçadas com pedaços de madeiras em nome da segurança." +
 	"\n\nSeu nome é Gary e você tem um casal de filhos pequenos: Kin e Jany." +
-	" Seu objetivo é protegê-los, até você ter os materiais necessários para construir um barco e tirá-los da ilha.";
+	" Seu objetivo é protegê-los, enquanto você reúne os materiais necessários para construir um barco e tirá-los da ilha." +
+	"\n\nVocê está em casa dormindo no momento..."+
+	"\nDica: Acorde o Gary!"
+	;
+
+MAX_X = 6;
+MAX_Y = 8; 
 
 function copy(text) {
 	historia = $("#historia").val();
 	$("#historia").val(historia + "\n" + text);
+}
+
+function andar(x, y, sentido){
+	// 0 - Norte, 1 - Leste, 2 - Sul, 3 - Oeste
+	if (sentido == 0) {
+		return [x, y + 1];
+	} else if (sentido == 1) {
+		return [x + 1, y];
+	} else if (sentido == 2){
+		return [x, y - 1];
+	} else {
+		return [x - 1, y];
+	};
 }
 
 
@@ -51,7 +70,7 @@ $(document).ready(function() {
 	 		}
 
 	 		if (chat.toLowerCase().indexOf("créditos") != -1) {
-	 			resposta = "\nEste jogo foi desenvolvido como parte de um projeto de férias por: Fernando Sarres & Ronald Kaiser";
+	 			resposta = "\nEste jogo foi desenvolvido como parte de um projeto de férias por: Fernando Sarres (sarresfernando@gmail.com) & Ronald Kaiser (raios.catodicos@gmail.com)";
 	 		};
 
 	 		if (chat.toLowerCase().indexOf("ajuda") != -1) {
